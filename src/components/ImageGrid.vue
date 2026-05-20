37b45bd1d17377519080612cbee935e753b2ce78
saschafuchs <54807121+saschafuchs@users.noreply.github.com>
2026-05-20 08:45:34 +0200

    feat: library grid 3→6 columns responsive (3/4/5/6)

--- src/components/ImageGrid.vue
+++ src/components/ImageGrid.vue
@@ -24,7 +24,7 @@ const emit = defineEmits<{
 </script>
 
 <template>
-  <div v-if="images.length" class="grid grid-cols-2 gap-4 xl:grid-cols-3 2xl:grid-cols-4">
+  <div v-if="images.length" class="grid grid-cols-3 gap-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6">
     <ImageCard
       v-for="image in images"
       :key="image.id"
