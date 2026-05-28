a14604583e825012f39a4378eed995ee62f2a911
saschafuchs <54807121+saschafuchs@users.noreply.github.com>
2026-05-28 09:42:41 +0200

    chore: remove temp build/commit helper scripts

--- do_commit.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-cd /Users/sascha.fuchs/develop/tools/crossposthelper
-git add -A
-git status --short
-git commit -m "feat: writing personas + X Premium+ + story narratives"
-echo "COMMIT_DONE:$?"
