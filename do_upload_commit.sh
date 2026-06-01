6a321cd27746909f0bd67dd759c3ce6cf8d417d1
saschafuchs <54807121+saschafuchs@users.noreply.github.com>
2026-06-01 10:25:02 +0200

    chore: remove upload helper scripts

--- do_upload_commit.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-cd /Users/sascha.fuchs/develop/tools/crossposthelper
-git add -A
-git commit -F cph_upload_commit.txt
-rm -f cph_upload_commit.txt
-git log --format="%h %s" -1
-echo "COMMIT_DONE"
