1ca7089fadd5d4bdd69679be0d17691d5a4a674f
saschafuchs <54807121+saschafuchs@users.noreply.github.com>
2026-06-12 13:15:59 +0200

    chore: release v0.2.124

--- src/data/changelog.ts
+++ src/data/changelog.ts
@@ -6,6 +6,13 @@ export interface ChangelogEntry {
 
 export const changelogEntries: ChangelogEntry[] = [
   {
+    version: "0.2.124",
+    date: "2026-06-12",
+    items: [
+      "Release v0.2.124",
+    ],
+  },
+  {
     version: "0.2.123",
     date: "2026-06-12",
     items: [
